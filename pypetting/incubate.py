3c109b61257846c12a041559545e819bb57e4d70
Daniel Angst <dcangst@gmail.com>
2021-10-22 13:15:56 +0200

    no space in facts parameter argument

--- pypetting/incubate.py
+++ pypetting/incubate.py
@@ -45,17 +45,17 @@ def read_plates_in_cartridge(cartridge):
 
 def present_plate(cartridge, position, labware):
     """Present plate"""
-    return _facts_storeX("StoreX_PresentPlate", f'"{cartridge}, {position}"', "1", labware)
+    return _facts_storeX("StoreX_PresentPlate", f'"{cartridge},{position}"', "1", labware)
 
 
 def return_plate(cartridge, position):
     """Present plate"""
-    return _facts_storeX("StoreX_ReturnPlate", f'"{cartridge}, {position}"', "1", "")
+    return _facts_storeX("StoreX_ReturnPlate", f'"{cartridge},{position}"', "1", "")
 
 
 def present_plate_by_bc(bc_file_path, bc_file_line, labware):
     """Present plate by barcode from file"""
-    return _facts_storeX("StoreX_PresentPlateByBarcode", f'"{bc_file_line}, {bc_file_path}"', "1", labware)
+    return _facts_storeX("StoreX_PresentPlateByBarcode", f'"{bc_file_line},{bc_file_path}"', "1", labware)
 
 
 def return_plate_by_bc():

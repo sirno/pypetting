9c4a051435a000bd376891ffbe8f522e9d3237d2
Nicolas Ochsner <nicolas.ochsner@env.ethz.ch>
2021-11-18 13:10:01 +0100

    Read infinite settings from xml file.

--- pypetting/infinite.py
+++ pypetting/infinite.py
@@ -1,5 +1,7 @@
 """Infinite Reader Commands."""
 
+import html
+
 from .misc import facts
 
 __all__ = [
@@ -19,6 +21,15 @@ def close_infinite_reader():
     return facts("ReaderNETwork", "ReaderNETwork_Close", "", 0, "")
 
 
-def measure_infinite_reader(path, settings):
+def measure_infinite_reader(path: str, settings_path: str):
     """Measure infinite reader."""
-    return facts("ReaderNETwork", "ReaderNETwork_Measure", f"{path}|{settings}", 0, "")
+    with open(settings_path, encoding="utf8") as settings_file:
+        settings = settings_file.read()
+
+    return facts(
+        "ReaderNETwork",
+        "ReaderNETwork_Measure",
+        f"{path}|{html.escape(settings)}",
+        0,
+        "",
+    )
